079f7f08153437f0e378bcc675cfca3d40d3ab2f
yjh0816 <2543kt@naver.com>
2023-05-15 15:22:54 +0900

    feat: Initialize router, vuex

--- frontend/cheerup/src/App.vue
+++ frontend/cheerup/src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <tool-bar></tool-bar>
+  <router-view/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToolBar from './components/ToolBar.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {ToolBar},
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+body {font-family: 'Playfair Display', serif; font-size:18px; padding:0; margin:0;}
+a {color: black;}
+* {margin:0; padding:0}
+li {list-style-type: none;}
+p + p {margin-top:20px}
+#app {padding:0 20px}
+.content {padding:30px 20px}
+
+.list li {padding-left:10px; list-style-type: none}
+.list li + li {margin-top:20px}
+.list li > a {text-decoration: none; font-size:22px}
+.list li > b {font-style: normal; font-size:12px; color:#999}
+.list li span {font-style: normal; font-weight:700; font-size:12px; color:#999;}
+.list li i {font-style: normal; font-size:12px; color:cornflowerblue}
 </style>
