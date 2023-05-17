0d14e60e56298e47c5dc2b094c2676b7670cffed
안성준 <dksckd0804@naver.com>
2023-05-17 15:27:24 +0900

    feat : append column cateory1, category2 at Table UserSeeker

--- backend/FinalSpring/src/main/java/com/service/spring/domain/UserSeekerVO.java
+++ backend/FinalSpring/src/main/java/com/service/spring/domain/UserSeekerVO.java
@@ -14,113 +14,184 @@ public class UserSeekerVO {
     private int limitId; //limit_id(fk)
     private String nation;
     private MultipartFile profile;
+    private int category1No; //category1_no
+    private int category2No; //category2_no
+    
 
     public UserSeekerVO() {
     }
 
-    public UserSeekerVO(String ename, String cname, int military, int bohun, int disabled, String nation, MultipartFile profile) {
-        this.ename = ename;
-        this.cname = cname;
-        this.military = military;
-        this.bohun = bohun;
-        this.disabled = disabled;
-        this.nation = nation;
-        this.profile = profile;
-    }
 
-    public int getSeekerId() {
-        return seekerId;
-    }
 
-    public void setSeekerId(int seekerId) {
-        this.seekerId = seekerId;
-    }
+	public UserSeekerVO(int seekerId, String ename, String cname, int military, int bohun, int disabled, int userId,
+			int limitId, String nation, MultipartFile profile, int category1No, int category2No) {
+		super();
+		this.seekerId = seekerId;
+		this.ename = ename;
+		this.cname = cname;
+		this.military = military;
+		this.bohun = bohun;
+		this.disabled = disabled;
+		this.userId = userId;
+		this.limitId = limitId;
+		this.nation = nation;
+		this.profile = profile;
+		this.category1No = category1No;
+		this.category2No = category2No;
+	}
 
-    public String getEname() {
-        return ename;
-    }
 
-    public void setEname(String ename) {
-        this.ename = ename;
-    }
 
-    public String getCname() {
-        return cname;
-    }
+	public int getSeekerId() {
+		return seekerId;
+	}
 
-    public void setCname(String cname) {
-        this.cname = cname;
-    }
 
-    public int getMilitary() {
-        return military;
-    }
 
-    public void setMilitary(int military) {
-        this.military = military;
-    }
+	public void setSeekerId(int seekerId) {
+		this.seekerId = seekerId;
+	}
 
-    public int getBohun() {
-        return bohun;
-    }
 
-    public void setBohun(int bohun) {
-        this.bohun = bohun;
-    }
 
-    public int getDisabled() {
-        return disabled;
-    }
+	public String getEname() {
+		return ename;
+	}
 
-    public void setDisabled(int disabled) {
-        this.disabled = disabled;
-    }
 
-    public int getUserId() {
-        return userId;
-    }
 
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
 
-    public int getLimitId() {
-        return limitId;
-    }
 
-    public void setLimitId(int limitId) {
-        this.limitId = limitId;
-    }
 
-    public String getNation() {
-        return nation;
-    }
+	public String getCname() {
+		return cname;
+	}
 
-    public void setNation(String nation) {
-        this.nation = nation;
-    }
 
-    public MultipartFile getProfile() {
-        return profile;
-    }
 
-    public void setProfile(MultipartFile profile) {
-        this.profile = profile;
-    }
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
 
-    @Override
-    public String toString() {
-        return "UserSeekerVO{" +
-                "seekerId=" + seekerId +
-                ", ename='" + ename + '\'' +
-                ", cname='" + cname + '\'' +
-                ", military=" + military +
-                ", bohun=" + bohun +
-                ", disabled=" + disabled +
-                ", userId=" + userId +
-                ", limitId=" + limitId +
-                ", nation='" + nation + '\'' +
-                ", profile=" + profile +
-                '}';
-    }
+
+
+	public int getMilitary() {
+		return military;
+	}
+
+
+
+	public void setMilitary(int military) {
+		this.military = military;
+	}
+
+
+
+	public int getBohun() {
+		return bohun;
+	}
+
+
+
+	public void setBohun(int bohun) {
+		this.bohun = bohun;
+	}
+
+
+
+	public int getDisabled() {
+		return disabled;
+	}
+
+
+
+	public void setDisabled(int disabled) {
+		this.disabled = disabled;
+	}
+
+
+
+	public int getUserId() {
+		return userId;
+	}
+
+
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+
+
+	public int getLimitId() {
+		return limitId;
+	}
+
+
+
+	public void setLimitId(int limitId) {
+		this.limitId = limitId;
+	}
+
+
+
+	public String getNation() {
+		return nation;
+	}
+
+
+
+	public void setNation(String nation) {
+		this.nation = nation;
+	}
+
+
+
+	public MultipartFile getProfile() {
+		return profile;
+	}
+
+
+
+	public void setProfile(MultipartFile profile) {
+		this.profile = profile;
+	}
+
+
+
+	public int getCategory1No() {
+		return category1No;
+	}
+
+
+
+	public void setCategory1No(int category1No) {
+		this.category1No = category1No;
+	}
+
+
+
+	public int getCategory2No() {
+		return category2No;
+	}
+
+
+
+	public void setCategory2No(int category2No) {
+		this.category2No = category2No;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "UserSeekerVO [seekerId=" + seekerId + ", ename=" + ename + ", cname=" + cname + ", military=" + military
+				+ ", bohun=" + bohun + ", disabled=" + disabled + ", userId=" + userId + ", limitId=" + limitId
+				+ ", nation=" + nation + ", profile=" + profile + ", category1No=" + category1No + ", category2No="
+				+ category2No + "]";
+	}
+	
 }
