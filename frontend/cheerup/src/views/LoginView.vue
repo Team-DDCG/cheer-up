74703b40956d44c73a085f64ff0b1a24c14ea4cc
YeonChanLim <yclim1010@naver.com>
2023-05-21 22:29:13 +0900

    feat: Update login view

--- frontend/cheerup/src/views/LoginView.vue
+++ frontend/cheerup/src/views/LoginView.vue
@@ -1,33 +1,24 @@
 <template>
-  <div style="background-color: #363533;">
+  <div class="main">
     <div id="innerBox">
 
-      <div class="title">
-        <h3 style="color: white">취얼업</h3>
-        <p style="font-size: 12px; color: white">귀찮은 자소서 작성을 한번에!</p>
+      <div class="main-title">
+
+        <div class="name">
+          <img src="../assets/logo.png"/>
+          <span class ="name1">취</span>
+          <span class ="name2">얼업</span>
+          
+          <p class ="sub-title">귀찮은 자소서 작성을 한번에!</p>
+        </div>
       </div>
       <div class="login-form">
-        <input class="form-control" type="text" id="id" v-model="id" placeholder="아이디">
-        <input class="form-control" type="password" id="password" v-model="password" placeholder="비밀번호">
-        <div
-          style="
-            display: flex;
-            justify-content: right;
-            align-items: center;
-            margin-top: 10px;
-            margin-right: 15px;
-          "
-        >
-          <span
-            style="
-              font-size: 12px;
-              margin-bottom: 8px;
-              margin-right: 5px;
-              color: white;
-            "
-            >Remember me</span
-          >
-          <img
+        <input class="form-control" type="text" id="id" v-model="id" placeholder="ID">
+        <input class="form-control" type="password" id="password" v-model="password" placeholder="Password">
+      </div>
+      <div class ="remember">
+       <input type="checkbox" name="xxx" value="yyy" checked>
+          <!-- <img
             @click="changeToggle"
             v-if="save_toggle === false"
             src="../assets/off-btn.png"
@@ -38,17 +29,19 @@
             v-else-if="save_toggle === true"
             src="../assets/on-btn.png"
             style="width: 80px; height: 40px"
-          />
+          /> -->
+          <span class="remember1">&nbsp;Remember me</span>
+          <span class="find">아이디/비밀번호찾기</span>
+          
         </div>
-      </div>
+      <div class="button">
       <div
-        @click="login" class="make-account-btn">Login</div>
-      <!-- <div class="make-account-btn">
-      <router-link  to="/register">회원가입하기</router-link>
-      </div> -->
-      <router-link class="make-account-btn" to="/register">회원가입하기</router-link>
+        @click="login" class="make-account-btn">login</div>
+      <router-link  to="/register"><div class="make-account-btn">회원가입</div></router-link>
+    </div>
     </div>
   </div>
+  
 </template>
 
 <script>
@@ -93,49 +86,141 @@ export default {
 </script>
 
 <style scoped>
-#innerBox {
-  background-color: #515151;
-  padding: 50px;
-  border-radius: 10px;
-  margin: 100px;
-  border: 1px solid #808080;
+img {
+  vertical-align:0% ;
 }
-.title {
-  margin: 40px;
-  margin-right: 0px;
-  margin-top: 0px;
-  margin-left: 30px;
-  text-align: left;
-  padding-top: 60px;
+.main {
+	width:100vw;
+	height:100vh;
+	display:flex;
+	justify-content:center;
+	align-items:center;
+  background-color: #363533;
+}
+#innerBox {
+ width: 450px;
+    height: 515px;
+    border-Radius: 16px;
+    background: #515151;
+    border: 1px solid #808080;
+}
+.main-title {
+  margin: 50px;
+  align-items: center;
   font-size: 18px;
   font-weight: 700;
-  color: #363533;
+  color: #f5f5f5;
+  background: #515151;
+}
+.main-title > h3{
+     width: 230;
+      height: 81;
+      left: 560;
+      top: 243;
+      font-Size: 64;
+      text-align: center;
+}
+.main-title > .name .name1 {
+  font-size: 64px;
+  font-weight: 700px;
+  text-align: center;
+  color: #a46cff;
 }
 
+.main-title > .name .name2 {
+   width: 230px;
+        height: 81px;
+        font-Size: 64px;
+        font-Weight: 700px;
+        text-Align: center;
+        color: #f5f5f5;
+}
+
+.main-title > .name{
+  margin: 0 auto;
+  text-align: center;
+  background: #515151;
+}
+
+.main-title > .name .sub_title{
+     width: 194;
+     height: 18.67px; 
+     font-Size: 16px; 
+     text-Align: center; 
+     color: #f5f5f5; 
+     
+}
+
+
 .make-account-btn {
-  width: 240px;
+  text-align: center;
+  width: 300px;
   height: 50px;
   border-radius: 30px;
   margin: 0 auto;
   border: 1px solid #363533;
+  border-radius: 13px;
   line-height: 50px;
   margin-bottom: 10px;
   background-color: #A46CFF; 
-  color: black
+  color: black;
 }
 
 .login-form {
-  margin-top: 80px;
+  width: 300px;
+  height: 42.01px;
+  margin: 0 auto;
+  margin-top: 40px;
+  align-items: center;
   margin-bottom: 90px;
 }
 
-.login-form > input {
-  width: 240px;
+.login-form > .form-control {
+  width: 300px;
+  height: 42.01px;
   border: none;
   border-bottom: 1px solid #363533;
-  background-color: white;
+  background-color: #f5f5f5;
+  border-radius: 13px;
+  margin-bottom: 20px;
   padding: 25px;
-  padding-bottom: 10px;
   outline: none;
 }
+.button {
+  display: block;
+  background: #515151;
+}
+.remember{
+  background: #515151;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+  margin-right: 15px;
+  font-Size: 14px;
+  font-Weight: 500;
+  text-Align: center;
+  color: #f5f5f5;
+}
+.remember > .remember1{
+  font-size: 14px;
+  display: flex;
+  margin-bottom: 8px;
+  margin-right: 5px;
+  color: #f5f5f5;
+}
+.remember > .find{
+  font-size: 14px;
+  display: flex;
+  margin-bottom: 8px;
+  margin-right: 5px;
+  color: #f5f5f5;
+}
+
+
+a {
+  text-decoration: none;
+}
+
+
 </style>
