73ef6d53a31229847af3f5badb181830a968f47a
miming48 <ma_onde@naver.com>
2023-05-25 14:37:33 +0900

    feat: Implement my cv view

---
+++ frontend/cheerup/src/components/SideBarCv.vue
@@ -0,0 +1,124 @@
+<template>
+    <div id="nav" class="navigator_cv">
+        <div id="sidenav_cv1" class="sidenav_cv">
+            <a href="#">개인정보</a>
+        </div>
+        <div id="sidenav_cv2" class="sidenav_cv">
+            <a href="#">학력정보</a>
+        </div>
+        <div id="sidenav_cv3" class="sidenav_cv">
+            <a href="#">경력사항</a>
+        </div>
+        <div id="sidenav_cv4" class="sidenav_cv">
+            <a href="#">프로젝트 경험</a>
+        </div>
+        <div id="sidenav_cv5" class="sidenav_cv">
+            <a href="#">대외 활동</a>
+        </div>
+        <div id="sidenav_cv6" class="sidenav_cv">
+            <a href="#">해외 경험</a>
+        </div>
+        <div id="sidenav_cv7" class="sidenav_cv">
+            <a href="#">어학 점수</a>
+        </div>
+        <div id="sidenav_cv8" class="sidenav_cv">
+            <a href="#">스킬</a>
+        </div>
+        <div id="sidenav_cv9" class="sidenav_cv">
+            <a href="#">자격증</a>
+        </div>
+        <div id="sidenav_cv10" class="sidenav_cv">
+            <a href="#">수상 경력</a>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+data() {
+    return {
+
+    }
+},
+
+}
+</script>
+<style scoped>
+
+
+/* .sidenav_cv {
+        height: 50px;
+        width: 163px;
+        position: relative;
+        top: 100px;
+        left: 50%;
+        margin: 10px;
+        background-color: #CCCCCC;
+        padding: 13px 8px 8px 32px;
+        font-size: 17px;
+        color: #818181;
+        display: block;
+        font-weight: lighter;
+    } */
+/* 
+a{
+ font-family: 'Montserrat', sans-serif; font-size:15px;
+  color: white;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  font-weight: lighter;
+  width: 100%;
+  padding: 5px 10px 5px 10px;
+  cursor: pointer;
+  width: 30%;
+  height: 50%;
+  margin-left: 2%;
+  margin-right: 2%;
+} */
+
+/* 
+a:hover{
+  font-family: 'Montserrat', sans-serif; font-size:15px;
+  color: white;
+  background-color: #A46CFF;
+  font-weight: bold;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 5px 10px 5px 10px;
+} 
+*/
+
+
+a{
+  font-family: 'Montserrat', sans-serif; font-size:13px;
+  color: white;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 10px;
+  cursor: pointer;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  margin-left: 50px;
+}
+a:hover{
+  font-family: 'Montserrat', sans-serif; font-size:13px;
+  color: white;
+  background-color: #A46CFF;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 5px 10px 5px 10px;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  margin-left: 50px;
+}
+
+#nav{
+    margin-top: 100px;
+    margin-bottom: 100px;
+}
+</style>
