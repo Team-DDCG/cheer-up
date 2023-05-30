7d146662b7a5d786ce51164256dcae60c033d699
YeonChanLim <yclim1010@naver.com>
2023-05-30 13:34:24 +0900

    feat: change text

--- frontend/cheerup/src/views/RegisterView1.vue
+++ frontend/cheerup/src/views/RegisterView1.vue
@@ -38,7 +38,7 @@
             </div>
           </label>
           <div class="agree">
-            <em>[필수]</em> 개인정보 동의서
+            <em>[필수]&nbsp;</em> 개인정보 동의서
           </div>
         </div>
         <label for="exampleFormControlInput1" class="form-label"></label>
@@ -68,7 +68,7 @@
             </div>
           </label>
           <div class="agree">
-            <em>[필수]</em> 이용약관 동의서
+            <em>[필수]&nbsp;</em> 이용약관 동의서
           </div>
         </div>
         <label for="exampleFormControlInput1" class="form-label"></label>
@@ -79,7 +79,7 @@
           id="exampleFormControlInput1"
           placeholder="Name"
         /> -->
-        <textarea v-model="privacyPolicy" class="form-control custom-scrollbar" id="exampleFormControlTextarea2" rows="10" 
+        <textarea v-model="privacyPolicy2" class="form-control custom-scrollbar" id="exampleFormControlTextarea2" rows="10" 
          style="height: 150px; white-space: pre-line; " readonly>
 
         </textarea>
@@ -112,6 +112,26 @@ export default {
       progressBarTransform: 'translateX(-55%)',
       agreement: '',
       privacyPolicy: `개인정보 동의서
+
+        취얼업(이하 '회사'라고 합니다)은 개인정보보호법 등 관련 법령상의 개인정보보호 규정을 준수하며 귀하의 개인정보보호에 최선을 다하고 있습니다. 회사는 개인정보보호법에 근거하여 다음과 같은 내용으로 개인정보를 수집 및 처리하고자 합니다. 다음의 내용을 자세히 읽어보시고 모든 내용을 이해하신 후에 동의 여부를 결정해주시기 바랍니다.
+
+        제1조(개인정보 수집 및 이용 목적)
+
+        이용자가 제공한 모든 정보는 다음의 목적을 위해 활용하며, 목적 이외의 용도로는 사용되지 않습니다.
+        - 본인확인 및 이력서, 자기소개서 작성 기능을 위한 정보
+
+
+        제2조(개인정보 수집 및 이용 항목)
+
+        회사는 개인정보 수집 목적을 위하여 다음과 같은 정보를 수집합니다.
+        - 성명, 주소, 전화번호, 이메일, 성별, 나이 및 생년월일
+
+
+        제3조(개인정보 보유 및 이용 기간)
+
+        수집한 개인정보는 수집·이용 동의일로부터 회원 탈퇴 시 까지 보관 및 이용합니다.
+        개인정보 보유기간의 경과, 처리목적의 달성 등 개인정보가 불필요하게 되었을 때에는 지체없이 해당 개인정보를 파기합니다.`,
+      privacyPolicy2: `이용약관 동의서
 
         취얼업(이하 '회사'라고 합니다)은 개인정보보호법 등 관련 법령상의 개인정보보호 규정을 준수하며 귀하의 개인정보보호에 최선을 다하고 있습니다. 회사는 개인정보보호법에 근거하여 다음과 같은 내용으로 개인정보를 수집 및 처리하고자 합니다. 다음의 내용을 자세히 읽어보시고 모든 내용을 이해하신 후에 동의 여부를 결정해주시기 바랍니다.
 
